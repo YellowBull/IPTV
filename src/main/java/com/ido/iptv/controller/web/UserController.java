1412c1255dd54d0f1184e84c3bae49ebfe5ba168
liaochonglc <1342709488@qq.com>
2018-10-04 23:21:34 +0800

    忘记加yml的了

---
+++ src/main/java/com/ido/iptv/controller/web/UserController.java
@@ -0,0 +1,14 @@
+package com.ido.iptv.controller.web;
+
+import io.swagger.annotations.Api;
+
+/**
+ * Web管理用户相关接口
+ *
+ * @author LiaoChong
+ * @date 2018-10-04 23:17
+ */
+@Api(tags = "后台管理的相关接口")
+public class UserController {
+
+}
