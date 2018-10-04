33dc68d9e8e01cb7dfed874a9cf65833e535af5e
liaochonglc <1342709488@qq.com>
2018-10-04 23:12:32 +0800

    两个组件放在test那里

---
+++ src/test/java/com/ido/iptv/StaticHtmlTest.java
@@ -0,0 +1,27 @@
+package com.ido.iptv;
+
+import freemarker.template.Template;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Map;
+
+@RestController
+public class StaticHtmlTest {
+    @Autowired
+    public Template template;
+@PostMapping("/user/testHtml")
+    public  String test(Map model) throws Exception{
+    System.out.println("------468464");
+        String path = "/static/login.html";
+        model.put("name","lc");
+    String p=this.getClass().getResource("/").toURI().getPath()+"static/login.html";
+    Writer file = new FileWriter(new File(p.substring(p.indexOf("/"))));
+        template.process(model,file);
+        return null;
+    }
+}
